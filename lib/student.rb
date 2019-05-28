035b85820b0235a8ec850f6f1a21eb64a6cf1205
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-28 20:59:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,11 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map {|row| new_from_db}first
+    DB[:conn].execute(sql, name).map {|row| new_from_db(row)}first
+  end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
 
 
